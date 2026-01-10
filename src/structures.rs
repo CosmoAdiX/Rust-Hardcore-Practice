987b99b42a65f9376c14c27f36f0f95ef092d8ed
RyzAdi <adityakumar03332@gmail.com>
2026-01-11 00:48:39 +0530

    updating structures

--- src/structures.rs
+++ src/structures.rs
@@ -1,13 +1,25 @@
 use colored::*;
 
+struct Rectangle {
+    length: u8,
+    width: u8,
+}
+
 pub fn structures() {
-    struct Recatangle {
-        length: u8,
-        width: u8,
-    }
-    println!("\n {}","|-->  Structures in Rust are like a way to group related data together, and use them throughout the program.".green());
+    
+    println!("\n{}","|-->  Structures in Rust are like a way to group related data together, and use them throughout the program.".green());
+
+    let new_rect = Rectangle {
+        length: 11,
+        width: 12,
+    };
+
+    let result_1 = rect_area(&new_rect); // here as you can see that the ownership is being transferred from the new_rect to rec and the rec to result_1 variable
+    println!("|-->  The area of the rectangle is {}.", result_1.to_string().green());
     
 }
 
-fn rect_area() {}
+fn rect_area(rec: &Rectangle) -> u8 {
+     return rec.length * rec.width;
+}
 
