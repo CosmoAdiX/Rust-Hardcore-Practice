e0188738ce7f5f70a596103ffb832e970e83c6ea
RyzAdi <adityakumar03332@gmail.com>
2026-01-12 23:37:28 +0530

    adding the methods as well

--- src/impl_and_associative.rs
+++ src/impl_and_associative.rs
@@ -15,6 +15,11 @@ impl Triangle {
             height: new_height,
         }
     }
+    // Method
+    fn area_traingle(&mut self) -> f64 {
+        (self.base as f64 * self.height as f64) / 2_f64
+    }
+    
 }
 
 pub fn impl_function() {
@@ -22,10 +27,17 @@ pub fn impl_function() {
 
     println!("\n |-->   This is an impl function and this is how we use it...");
 
-    let new_tria = Triangle::new(120,66);
 
-    println!(" |-->   {:?}", new_tria);
+    // Now we can use our initialization of the triangle from anywhere by Triangle::new() and saving it in the variable and all.
+    let mut new_tria = Triangle::new(27,37);
 
+    // println!(" |-->   {:?}", new_tria);
+
+
+    let v = new_tria.area_traingle();
+
+    println!(" |-->    Soo the area of the triangle is: {} unit.", v);
     
 }
+
 
